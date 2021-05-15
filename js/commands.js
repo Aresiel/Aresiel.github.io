c0ab40fd8a14dc29ee7ac9a2ca246db04fd5ba16
Aresiel <love.dittlau@gmail.com>
2021-05-15 15:34:36 +0200

    Fixed animation.

--- js/commands.js
+++ js/commands.js
@@ -12,12 +12,10 @@ function help(args){
 function exit(args){
     terminal_content.parentElement.style.animationName = "fade_out"
     terminal_content.parentElement.style.animationDuration = "1s"
-    document.getElementsByTagName("body")[0].style.animationName = "fade_out"
-    document.getElementsByTagName("body")[0].style.animationDuration = "1s"
 
     is_prompt = false
     setTimeout(() => {
-        document.getElementsByTagName("body")[0].remove()
+        terminal_content.parentElement.remove()
     }, 2000)
 }
 
